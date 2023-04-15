e9feb8ae9875072fc9dba07a4e05a5b6aa4faa0e
maciej.szafranski <maciej.szafranski@schulz-infoprod.pl>
2023-04-15 23:38:23 +0200

    backup 2023-04-15 23:38

---
+++ AdvertisingPortal/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvertisingPortal.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
